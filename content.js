a35785b87f520636b18faaa578c32cc9deb782ca
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-03-24 12:04:44 +0530

    merged code of meet attendance tracker and screen recorder

--- content.js
+++ content.js
@@ -1,184 +1,442 @@
 window.addEventListener("load", () => {
   console.log("this is the content.js file of extension");
-});
+  let stream = null;
+  let audio = null;
+  let mixedStream = null;
+  let chunks = [];
+  let recorder = null;
+  let isRecordingVideo = false;
 
-let stream = null,
-  audio = null,
-  mixedStream = null,
-  chunks = [],
-  recorder = null;
-(startButton = null),
-  (stopButton = null),
-  (downloadButton = null),
-  (recordedVideo = null);
+  //attendance tracker variables
 
-let isRecordingVideo = false;
+  let studentDetails = new Map();
+  let studentsNameSet = new Set();
+  let ui_buttons;
+  let totalClassDuration = 0;
+  let goingToStop = 0;
+  let isAttendanceWorking = false;
+  let buttonClickInd = 0;
+  let startTime;
+  let flag = true; // make if false to block non-meraki classes
+  let meetingDuration;
+  const redirectUrl = "http://192.168.101.4:5500/index.html";
+  // let newWindow1 = window.open(redirectUrl);
 
-let videoDisplay = document.createElement("video");
-videoDisplay.classList.add("video-feedback");
+  let meetingCode = window.location.pathname.substring(1);
+  let date = new Date();
+  let dd = date.getDate();
+  let mm = date.toLocaleString("default", { month: "short" });
+  let yyyy = date.getFullYear();
+  date = dd + "-" + mm + "-" + yyyy;
+  let sortedtstudentsNameSet = [];
+  let studentsAttendedDuration = [];
+  //   let studentsJoiningTime = [];
+  let mapKeys = studentDetails.keys();
 
-// var stopButton = document.querySelector(".Gt6sbf");
+  // let videoDisplay = document.createElement("video");
+  // videoDisplay.classList.add("video-feedback");
 
-// stopButton.addEventListener("click", stopRecording);
+  let recButton = document.createElement("button");
+  recButton.id = "recButton";
+  // recButton.className = "Jyj1Td CkXZgc";
+  recButton.type = "button";
+  recButton.innerHTML = "Rec";
+  recButton.style.border = "none";
+  recButton.style.backgroundColor = "#ea4335";
+  recButton.style.color = "white";
+  recButton.style.height = "2.6rem";
+  recButton.style.width = "2.5rem";
+  recButton.style.borderRadius = "50%";
+  recButton.style.cursor = "pointer";
 
-let newButton = document.createElement("button");
-newButton.id = "newButton";
-newButton.className = "Jyj1Td CkXZgc";
-newButton.type = "button";
-newButton.innerHTML = "Rec";
-newButton.style.border = "none";
-newButton.style.backgroundColor = "#ea4335";
-newButton.style.color = "white";
-newButton.style.height = "2.6rem";
-newButton.style.width = "4.2rem";
-newButton.style.borderRadius = "50%";
-newButton.style.cursor = "pointer";
+  // Adding Recording button to meet ui
+  let newButton = document.createElement("button");
+  newButton.id = "newButton";
+  newButton.className = "Jyj1Td CkXZgc";
+  newButton.type = "button";
+  newButton.innerHTML = "Record";
+  newButton.style.border = "none";
+  newButton.style.backgroundColor = "#ea4335";
+  newButton.style.color = "white";
+  newButton.style.height = "2.6rem";
+  newButton.style.width = "4.2rem";
+  newButton.style.borderRadius = "30px";
 
+  recButton.addEventListener("click", () => {
+    if (recButton.innerHTML == "Rec") {
+      isRecordingVideo = true;
+      recButton.innerHTML = "Stop";
+    } else if (recButton.innerHTML == "Stop") {
+      isRecordingVideo = false;
+      recButton.innerHTML = "Rec";
+    }
 
-if (isRecordingVideo === true) {
-  newButton.disabled = true;
-}
+    if (isRecordingVideo) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  });
 
-function insertButton() {
-  try {
-    ui_buttons = document.getElementsByClassName("VfPpkd-kBDsod NtU4hc");
-    // ui_buttons[1].click();
-    // console.log("getting in try block of insert button");
-    document.getElementsByClassName("Tmb7Fd")[0].appendChild(newButton);
-  } catch (error) {
-    // console.log("error coming from error block", error);
+  function insertRecButton() {
+    try {
+      ui_buttons = document.getElementsByClassName("VfPpkd-kBDsod NtU4hc");
+
+      document.getElementsByClassName("jsNRx")[0].appendChild(recButton);
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
 
-setInterval(insertButton, 1000);
+  let insertBtnInterval = setInterval(() => {
+    insertRecButton();
+  }, 1000);
 
-// startButton = document.querySelector(".start-recording");
-// stopButton = document.querySelector(".stop-recording");
+// if(document.getElementsByClassName("VfPpkd-kBDsod NtU4hc")){
+//   setTimeout(insertRecButton(),1000)
+// }
 
-async function setupStream() {
-  try {
-    stream = await navigator.mediaDevices.getDisplayMedia({
-      video: true,
+
+  function insertButton() {
+    try {
+      ui_buttons = document.getElementsByClassName("VfPpkd-kBDsod NtU4hc");
+      // ui_buttons[1].click();
+      document.getElementsByClassName("Tmb7Fd")[0].appendChild(newButton);
+      if (!isAttendanceWorking) {
+        isAttendanceWorking = true;
+        newButton.style.backgroundColor = "#00796b";
+        StartTime = new Date().toLocaleTimeString();
+        studentDetails.clear();
+        studentsNameSet.clear();
+        totalClassDuration = 0;
+        start();
+      }
+
+      document
+        .getElementsByClassName("Gt6sbf QQrMi")
+        .addEventListener("click", function () {
+          if (isAttendanceWorking) {
+            stop();
+          }
+        });
+      clearInterval(tryInsertingButton);
+    } catch (error) {}
+  }
+
+  async function merakiClassChecker(url) {
+    const API_URL = "https://dev-api.navgurukul.org/classes";
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM5Nzc2IiwiZW1haWwiOiJzaGl2YW5zaEBuYXZndXJ1a3VsLm9yZyIsImlhdCI6MTY3OTAzNTE4OCwiZXhwIjoxNzEwNTkyNzg4fQ.Ayzgfkk9k6PE_kaybCAznNeEXmF01zp7pLa5zOQ0f4k";
+
+    const data = await fetch(API_URL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "version-code": 99,
+      },
     });
-
-    audio = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
-
-    setupVideoFeedback();
-  } catch (err) {
-    console.log(err);
+    const parsed_data = await data.json();
+    for (let ind = 0; ind < parsed_data.length; ind++) {
+      if (parsed_data[ind].meet_link === url) {
+        flag = true;
+        break;
+      }
+    }
+    return flag;
   }
-}
 
-function stopRecording() {
-  console.log("recording stopped ");
-  recorder.stop();
-  //   recorder.onstop = handleStop;
-  // console.log(chunks);
-}
+  let meet_url = window.location.href;
+  const checked_url = merakiClassChecker(meet_url);
+  checked_url.then((result) => {
+    if (result) {
+      setInterval(insertButton, 1000);
+    }
+  });
 
-function setupVideoFeedback() {
-  // newButton.style.display = 'none'
-  if (stream) {
-    // const video = document.querySelector(".video-feedback");
-    // video.srcObject = stream;
-    // video.play();
-    console.log(stream, "This is stream");
-  } else {
-    console.log("No stream available");
+  async function start() {
+    startTime = new Date();
+    startAttendanceTracker = setInterval(attendanceTracker, 1000);
   }
-}
 
-async function startRecording() {
-  isRecordingVideo = true;
-  if (isRecordingVideo === true) {
-    console.log("recording will not work again")
-    newButton.disabled = true;
+  // to get the meeting name/title
+  const getMeetingName = () => {
+    const elm = document.querySelector("[data-meeting-title]");
+    if (elm && elm.dataset.meetingTitle) {
+      return elm.dataset.meetingTitle;
+    }
+    return document.title;
+  };
+
+  let stop = (STOP = () => {
+    let newWindow1 = window.open(redirectUrl);
+
+    clearInterval(startAttendanceTracker);
+
+    //   let studentsJoiningTime = [];
+    let mapKeys = studentDetails.keys();
+    for (i = 0; i < studentDetails.size; i++) {
+      let studentName = mapKeys.next().value;
+      sortedtstudentsNameSet.push(studentName);
+    }
+    sortedtstudentsNameSet.sort();
+    for (studentName of sortedtstudentsNameSet) {
+      let data = studentDetails.get(studentName);
+      studentsAttendedDuration.push(data[0].toString());
+      // studentsJoiningTime.push(data[1]);
+    }
+    const end_time = new Date();
+
+    var record = {
+      attendee_names: JSON.stringify(sortedtstudentsNameSet),
+      attendedDurationInSec: JSON.stringify(studentsAttendedDuration),
+      meet_code: meetingCode,
+      meeting_title: getMeetingName().replace("Meet - ", ""),
+      meeting_time: startTime.toISOString(),
+    };
+
+    setTimeout(() => {
+      const api = "https://192.168.101.4:5500/index.html"; // endpoint where this data will go
+      fetch(api, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(record),
+      })
+        .then((response) => response.json())
+        .then((string) => {
+          console.log(`Title of our response :  ${string.title}`);
+          window.open("http://192.168.101.4:5500/index.html");
+          // runtime.openOptionsPage()
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 2000);
+
+    record.meet_duration = meetingDuration;
+    console.log(record, "Attendance Record");
+
+    let newRecord;
+
+    const oldRecord = chrome.storage.sync.get(
+      "Meraki_Attendance_Record",
+      (data) => {
+        const oldData = data.Meraki_Attendance_Record;
+
+        if (!oldData) {
+          const setData = chrome.storage.sync.set({
+            Meraki_Attendance_Record: [record],
+          });
+        } else {
+          oldData.push(record);
+          chrome.storage.sync.set({
+            Meraki_Attendance_Record: oldData,
+          });
+        }
+      }
+    );
+
+    setTimeout(function () {
+      newWindow1.postMessage(JSON.stringify(record), redirectUrl);
+    }, 10000);
+  });
+
+  function attendanceTracker() {
+    let currentlyPresentStudents = document.getElementsByClassName("zWGUib");
+    if (currentlyPresentStudents.length > 0) {
+      studentsNameSet.clear();
+      let numberOfjoinedStudents = -1;
+      try {
+        numberOfjoinedStudents = Number(
+          document.getElementsByClassName("uGOf1d")[1].innerHTML
+        );
+        numberOfjoinedStudents =
+          Number.isInteger(numberOfjoinedStudents) &&
+          numberOfjoinedStudents > 0 &&
+          numberOfjoinedStudents != -1
+            ? numberOfjoinedStudents
+            : currentlyPresentStudents.length;
+      } catch (e) {
+        numberOfjoinedStudents = currentlyPresentStudents.length;
+      }
+      for (i = 0; i < numberOfjoinedStudents; i++) {
+        try {
+          studentsNameSet.add(
+            currentlyPresentStudents[i].innerHTML.toUpperCase()
+          );
+        } catch (exception) {}
+      }
+      for (studentName of studentsNameSet) {
+        if (studentDetails.has(studentName)) {
+          let data = studentDetails.get(studentName);
+          data[0] += 1;
+          studentDetails.set(studentName, data);
+        } else {
+          let joiningTime = new Date().toLocaleTimeString();
+          let currStatus = 1;
+          let data = [];
+          data.push(currStatus);
+          data.push(joiningTime);
+          studentDetails.set(studentName, data);
+        }
+      }
+      if (studentsNameSet.size - 1 == -1) {
+        goingToStop += 1;
+      } else {
+        meetingDuration = toTimeFormat(totalClassDuration);
+        newButton.innerHTML = toTimeFormat(totalClassDuration);
+        totalClassDuration += 1;
+        goingToStop = 0;
+      }
+      if (goingToStop == 2) {
+        isAttendanceWorking = false;
+        newButton.innerHTML = "Track Attendance";
+        newButton.style.border = "2px solid #C5221F";
+        goingToStop = 0;
+        stop();
+      }
+    } else {
+      try {
+        // ui_buttons[buttonClickInd % ui_buttons.length].click();
+        ui_buttons[1].click();
+        buttonClickInd += 1;
+        goingToStop = 0;
+      } catch (error) {
+        goingToStop += 1;
+        if (goingToStop == 2) {
+          isAttendanceWorking = false;
+          newButton.innerHTML = "Track Attendance";
+          newButton.style.border = "2px solid #C5221F";
+          goingToStop = 0;
+          stop();
+        }
+      }
+    }
+  }
+  function toTimeFormat(time) {
+    const SECONDS_IN_HOUR = 3600;
+    const SECONDS_IN_MINUTE = 60;
+  
+    let hours = Math.floor(time / SECONDS_IN_HOUR);
+    let minutes = Math.floor((time % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE);
+    let seconds = time % SECONDS_IN_MINUTE;
+  
+    hours = hours.toString().padStart(2, "0");
+    minutes = minutes.toString().padStart(2, "0");
+    seconds = seconds.toString().padStart(2, "0");
+  
+    return hours === "00"
+      ? `${minutes}:${seconds}`
+      : `${hours}:${minutes}:${seconds}`;
   }
 
   
-  await setupStream();
-  console.log("Recorder function is running");
-  if (stream && audio) {
-    mixedStream = new MediaStream([
-      ...stream.getTracks(),
-      ...audio.getTracks(),
-    ]);
 
-    recorder = new MediaRecorder(mixedStream);
+  //Recorder functions
 
-    recorder.ondataavailable = handleDataAvailable;
-    recorder.start(1000);
+  async function setupStream() {
+    try {
+      stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
 
-    recorder.onstop = handleStop;
-    // setInterval(insertButton, 1000);
+      audio = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
 
-    console.log("Recording started");
-  } else {
-    console.log("No stream available.");
+      setupVideoFeedback();
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
 
-console.log(newButton, "this is new Button");
 
-newButton.addEventListener("click", startRecording);
+  async function stopRecording() {
+    const blob = new Blob(chunks, { type: "video/mp4" });
+    // console.log(JSON.parse(JSON.stringify(chunks)), "string chunks");
 
-function handleDataAvailable(e) {
-  newButton.disabled = true;
-  console.log(chunks, "this is chunks");
-  chunks.push(e.data);
-}
+    console.log(blob, "the orignal blob recieved");
+    let base64Video = [];
 
-function handleStop(e) {
-  //   console.log("recording stopped");
-  //   const blob = new Blob(chunks, { type: "video/mp4" });
-  //   chunks = [];
+    // Function to convert a single Blob to Base64
+    const blobToBase64 = (blob) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = () => {
+          const base64String = reader.result
+            .toString()
+            .replace(/^data:(.*,)?/, "");
+          resolve(base64String);
+        };
+        reader.onerror = (error) => reject(error);
+      });
+    };
 
-  //   downloadButton.href = URL.createObjectURL(blob);
-  //   downloadButton.download = "video.mp4";
-  //   downloadButton.disabled = false;
+    // Use Promise.all() to convert the array of Blobs to an array of Base64 strings
+    blobToBase64(blob)
+      .then((base64Strings) => {
+        base64Video = base64Strings;
+        console.log(base64Strings, "converting blob to base 64")
+        // const url = `https://shivansh3218-fantastic-spoon-rj9qvj64pjfp77v-5500.preview.app.github.dev/index.html?base64=${base64Video}`; // endpoint where this data will go
+        // console.log(url, "url of the next page");
 
-  //   recordedVideo.src = URL.createObjectURL(blob);
-  //   recordedVideo.load();
-  //   recordedVideo.onloadeddata = function () {
-  //     const rc = document.querySelector(".recorded-video-wrap");
-  //     rc.classList.remove("hidden");
-  //     rc.scrollIntoView({ behavior: "smooth", block: "start" });
+        // console.log(base64Video, "base64 video outside then");
+        // window.open(url);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-  //     recordedVideo.play();
-  //   };
+    recButton.href = URL.createObjectURL(blob);
+    recButton.download = "video.mp4";
+    console.log("recording stopped stopRecording function ");
+    recorder.stop();
+    recorder.onstop = handleStop;
+  }
 
-  //   stream.getTracks().forEach((track) => track.stop());
-  //   audio.getTracks().forEach((track) => track.stop());
-  clearInterval(insertButton);
-  console.log("Recording stopped");
-  isRecordingVideo = false;
-}
+  function setupVideoFeedback() {
+    if (stream) {
+      console.log(stream, "This is stream");
+    } else {
+      console.log("No stream available");
+    }
+  }
 
-// window.addEventListener('load', () => {
+  async function startRecording() {
+    await setupStream();
+    console.log("Recorder function is running");
+    if (stream && audio) {
+      mixedStream = new MediaStream([
+        ...stream.getTracks(),
+        ...audio.getTracks(),
+      ]);
 
-// 	downloadButton = document.querySelector('.download-video');
-// 	recordedVideo = document.querySelector('.recorded-video');
+      recorder = new MediaRecorder(mixedStream);
 
-//   startButton.addEventListener("click", startRecording);
-//   stopButton.addEventListener("click", stopRecording);
-//   })
-// });
-// VfPpkd-Bz112c-LgbsSe    yHy1rc
+      recorder.ondataavailable = handleDataAvailable;
+      recorder.start(1000);
+      recorder.onstop = stopRecording;
+      console.log("Recording started");
+    } else {
+      console.log("No stream available.");
+    }
+  }
 
-// var recorder = new RecordRTC_Extension();
+  function handleDataAvailable(e) {
+    console.log(chunks, "this is chunks");
 
-// recorder.startRecording({
-//     enableScreen: true,
-//     enableMicrophone: true,
-//     enableSpeakers: true
-// });
+    if (e.data) {
+      chunks.push(e.data);
+    }
+  }
 
-// btnStopRecording.onclick = function() {
-//     recorder.stopRecording(function(blob) {
-//         console.log(blob.size, blob);
-//         var url = URL.createObjectURL(blob);
-//         video.src = url;
-//     });
-// };
+  function handleStop(e) {
+    clearInterval(insertBtnInterval);
+    console.log("Recording stopped handleStop function");
+  }
+});
+
+
