f83910a93070df83282c681352e8efcb9ff340b3
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-04-02 16:41:01 +0530

    Updated screen recorder

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  alert("Hello dom is loaded");
-  console.log(document.getElementById("start-recording"));
-//   var button = document.getElementById("start-recording");
-//   button.addEventListener("click", function () {
-//     console.log("script is woerking");
-//     alert("Hello, world!");
-//   });
-});
